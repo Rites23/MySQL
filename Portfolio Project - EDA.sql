c9106ab2593e99926fc2e8d8df9d3760b133c2fa
Ritesh Balu <76614949+Rites23@users.noreply.github.com>
2024-10-19 12:19:36 +0200

    Update Portfolio Project - EDA.sql

--- Portfolio Project - EDA.sql
+++ Portfolio Project - EDA.sql
@@ -1,10 +1,10 @@
--- EDA
+-- Exploratory Data Analysis (EDA)
 
--- Here we are jsut going to explore the data and find trends or patterns or anything interesting like outliers
+-- In this phase, we will explore the data to identify trends, patterns, and any interesting insights, including potential outliers.
 
--- normally when you start the EDA process you have some idea of what you're looking for
+-- Typically, when initiating the EDA process, I may have specific hypotheses or questions in mind. 
 
--- with this info we are just going to look around and see what we find!
+-- However, in this instance, I will adopt a more exploratory approach, examining the data broadly to uncover any noteworthy findings!
 
 SELECT * 
 FROM world_layoffs.layoffs_staging2;
@@ -15,54 +15,36 @@ SELECT MAX(total_laid_off)
 FROM world_layoffs.layoffs_staging2;
 
 
-
-
-
-
--- Looking at Percentage to see how big these layoffs were
+-- Looking at Percentage to see how big these layoffs were:
 SELECT MAX(percentage_laid_off),  MIN(percentage_laid_off)
 FROM world_layoffs.layoffs_staging2
 WHERE  percentage_laid_off IS NOT NULL;
 
--- Which companies had 1 which is basically 100 percent of they company laid off
+-- Which companies had 1 which is basically 100 percent of they company laid off:
 SELECT *
 FROM world_layoffs.layoffs_staging2
 WHERE  percentage_laid_off = 1;
--- these are mostly startups it looks like who all went out of business during this time
+-- These are mostly startups, it looks like who all went out of business during this time
 
--- if we order by funcs_raised_millions we can see how big some of these companies were
+-- If I order by funcs_raised_millions I can see how big some of these companies were
 SELECT *
 FROM world_layoffs.layoffs_staging2
 WHERE  percentage_laid_off = 1
 ORDER BY funds_raised_millions DESC;
--- BritishVolt looks like an EV company, Quibi! I recognize that company - wow raised like 2 billion dollars and went under - ouch
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+-- BritishVolt appears to be an EV company, while Quibi—having raised nearly $2 billion—unfortunately went under. A stark reminder of the tech industry's volatility!
 
 
 -- SOMEWHAT TOUGHER AND MOSTLY USING GROUP BY--------------------------------------------------------------------------------------------------
 
--- Companies with the biggest single Layoff
+-- Companies with the biggest single Layoff:
 
 SELECT company, total_laid_off
 FROM world_layoffs.layoffs_staging
 ORDER BY 2 DESC
 LIMIT 5;
--- now that's just on a single day
+-- Now that's just on a single day
 
--- Companies with the most Total Layoffs
+-- Companies with the most Total Layoffs:
 SELECT company, SUM(total_laid_off)
 FROM world_layoffs.layoffs_staging2
 GROUP BY company
@@ -70,15 +52,14 @@ ORDER BY 2 DESC
 LIMIT 10;
 
 
-
--- by location
+-- By location
 SELECT location, SUM(total_laid_off)
 FROM world_layoffs.layoffs_staging2
 GROUP BY location
 ORDER BY 2 DESC
 LIMIT 10;
 
--- this it total in the past 3 years or in the dataset
+-- This it total in the past 3 years or in the dataset
 
 SELECT country, SUM(total_laid_off)
 FROM world_layoffs.layoffs_staging2
@@ -103,14 +84,10 @@ GROUP BY stage
 ORDER BY 2 DESC;
 
 
-
-
-
-
 -- TOUGHER QUERIES------------------------------------------------------------------------------------------------------------------------------------
 
--- Earlier we looked at Companies with the most Layoffs. Now let's look at that per year. It's a little more difficult.
--- I want to look at 
+-- Earlier I looked at Companies with the most Layoffs. Now let's look at that per year. 
+-- I want to look at:
 
 WITH Company_Year AS 
 (
@@ -129,15 +106,13 @@ AND years IS NOT NULL
 ORDER BY years ASC, total_laid_off DESC;
 
 
-
-
--- Rolling Total of Layoffs Per Month
+-- Rolling Total of Layoffs Per Month:
 SELECT SUBSTRING(date,1,7) as dates, SUM(total_laid_off) AS total_laid_off
 FROM layoffs_staging2
 GROUP BY dates
 ORDER BY dates ASC;
 
--- now use it in a CTE so we can query off of it
+-- Now use it in a CTE so I can query off of it:
 WITH DATE_CTE AS 
 (
 SELECT SUBSTRING(date,1,7) as dates, SUM(total_laid_off) AS total_laid_off
@@ -148,51 +123,6 @@ ORDER BY dates ASC
 SELECT dates, SUM(total_laid_off) OVER (ORDER BY dates ASC) as rolling_total_layoffs
 FROM DATE_CTE
 ORDER BY dates ASC;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
